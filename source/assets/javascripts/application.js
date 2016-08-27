55d4f8dc56d30df891f41c96cb9f5b3030de0cbb
htomine <htomine@gmail.com>
2016-08-27 21:24:35 +0900

    Rewrite as pmconf

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -3,6 +3,6 @@
 
 //= require_tree .
 
-if (location.hostname == 'htomine.github.io') {
-    location.href = 'http://johokaigi.org'
-}
+// if (location.hostname == 'htomine.github.io') {
+//     location.href = 'http://pmconf.org'
+// }
